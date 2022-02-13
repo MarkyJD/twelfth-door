a9ba750cadf284673f6ad7d7cd6e47630a72e59c
MarkyJD <marky.dodson@gmail.com>
2022-02-13 22:21:45 +1100

    feat: Select styled and working

--- src/components/TextEditor/RecipientsWidget.js
+++ src/components/TextEditor/RecipientsWidget.js
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Select from 'react-select';
 import Field from './Field';
 
-function upperFirstLetter(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-export default function RecipientsWidget({
-  name,
-  label,
-  value,
-  onChange,
-  options,
-}) {
+const customStyles = {
+  control: (provided, state) => ({
+    backgroundColor: 'transparent',
+    display: 'flex',
+    color: '#bfc4cb',
+    fontSize: '1.125rem',
+    lineHeight: '1.75rem',
+    fontWeight: '400',
+  }),
+};
+
+export default function RecipientsWidget({ name, label, onChange, options }) {
   const [focus, setFocus] = useState();
   const users = [
     {
@@ -20,21 +22,33 @@ export default function RecipientsWidget({
       label: 'everyone',
     },
   ];
-  options.forEach((user) => {
-    users.push({
-      value: user.username,
-      label: user.username,
+
+  useEffect(() => {
+    onChange(users);
+  }, []);
+
+  options
+    .sort((a, b) => a.username.localeCompare(b.username))
+    .forEach((user) => {
+      users.push({
+        value: user.username,
+        label: user.username,
+      });
     });
-  });
 
   return (
     <Field focus={focus} label={label} setFocus={setFocus}>
       <Select
-        className="border-none"
+        className="text-slate-900 font-normal ml-[-10px] "
         options={users}
         defaultValue={[users[0]]}
         isMulti
         closeMenuOnSelect={false}
+        styles={customStyles}
+        onMenuOpen={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        onChange={(value) => onChange(value)}
+        placeholder={name}
       />
     </Field>
   );
@@ -43,7 +57,6 @@ export default function RecipientsWidget({
 RecipientsWidget.propTypes = {
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  value: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   options: PropTypes.array.isRequired,
 };
--- src/components/TextEditor/index.js
+++ src/components/TextEditor/index.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { BiX } from 'react-icons/bi';
 import DebugWidget from './DebugWidget';
 import MarkdownWidget from './MarkdownWidget';
 import TextWidget from './TextWidget';
 import RecipientsWidget from './RecipientsWidget';
 import { getAllUsers } from '../../services/firebase-services';
 
-export default function TextEditor() {
+export default function TextEditor({ toggleEditor, isEditorOpenOnMobile }) {
   const [value, setValue] = useState({
     entry: {},
     debug: true,
@@ -36,7 +38,18 @@ export default function TextEditor() {
 
   return (
     users && (
-      <>
+      <div className="h-full overflow-auto">
+        <div className="py-1 mb-1 px-5 mx-auto flex items-center justify-between h-100 overflow-auto">
+          <h2 className="text-xl font-thin font-title text-slate-800 dark:text-slate-200 text-shadow-none">
+            Whats on your mind?
+          </h2>
+          <BiX
+            className="icon text-black dark:text-white hover:bg-slate-200 dark:hover:bg-slate-600 rounded"
+            onClick={() => toggleEditor(isEditorOpenOnMobile)}
+          />
+        </div>
+        <hr />
+
         <TextWidget
           title
           name="Enter a title or subject"
@@ -45,7 +58,7 @@ export default function TextEditor() {
           onChange={(title) => updateValue({ title })}
         />
         <TextWidget
-          name="(optional) Enter a subtitle"
+          name="Enter a subtitle (optional)"
           label="subtitle"
           value={entry.subtitle}
           onChange={(subtitle) => updateValue({ subtitle })}
@@ -59,7 +72,12 @@ export default function TextEditor() {
         />
         <MarkdownWidget />
         {debug && <DebugWidget data={entry} />}
-      </>
+      </div>
     )
   );
 }
+
+TextEditor.propTypes = {
+  toggleEditor: PropTypes.func.isRequired,
+  isEditorOpenOnMobile: PropTypes.bool.isRequired,
+};
