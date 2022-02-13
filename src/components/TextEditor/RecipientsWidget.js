3520a951abd3ad4e17ae7bd75890a151cc7d9b70
MarkyJD <54533821+MarkyJD@users.noreply.github.com>
2022-02-13 17:06:32 +1100

    feat: Add react select

--- src/components/TextEditor/RecipientsWidget.js
+++ src/components/TextEditor/RecipientsWidget.js
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import Select from 'react-select';
 import Field from './Field';
 
+function upperFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 export default function RecipientsWidget({
   name,
   label,
@@ -10,14 +14,28 @@ export default function RecipientsWidget({
   options,
 }) {
   const [focus, setFocus] = useState();
+  const users = [
+    {
+      value: '@everyone',
+      label: 'everyone',
+    },
+  ];
+  options.forEach((user) => {
+    users.push({
+      value: user.username,
+      label: user.username,
+    });
+  });
 
   return (
     <Field focus={focus} label={label} setFocus={setFocus}>
-      <select>
-        {options.map((opt) => (
-          <option key={opt.userId}>{opt.username}</option>
-        ))}
-      </select>
+      <Select
+        className="border-none"
+        options={users}
+        defaultValue={[users[0]]}
+        isMulti
+        closeMenuOnSelect={false}
+      />
     </Field>
   );
 }
